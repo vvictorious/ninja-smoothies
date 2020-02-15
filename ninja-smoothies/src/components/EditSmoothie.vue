99d6caf71d171d8a38e3252142b5a8c3e552857a
vvictorious <vvictorious30@gmail.com>
2020-02-15 12:03:40 -0700

    edit smoothie is updating on the db when update smoothie btn gets clicked

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -25,7 +25,7 @@
 
 <script>
 import db from '@/firebase/init'
-
+import slugify  from 'slugify'
 export default {
     name: 'EditSmoothie',
     data () {
@@ -37,7 +37,26 @@ export default {
     },
     methods: {
         EditSmoothie () {
-            console.log(this.smoothie.title, this.smoothie.ingredients)
+            if (this.smoothie.title) {
+                this.feedback = null
+                //create slug
+                this.smoothie.slug = slugify(this.smoothie.title, {
+                    replacement: '-',
+                    remove: /[$*_+~.()'"!\-:@]/g,
+                    lower: true
+                })
+                db.collection('smoothies').doc(this.smoothie.id).update({
+                    title: this.smoothie.title,
+                    ingredients: this.smoothie.ingredients,
+                    slug: this.smoothie.slug              
+                }).then(() => {
+                    this.$router.push({ name: 'Index' })
+                }).catch(err => {
+                    console.log(err)
+                })
+            } else {
+                this.feedback = 'you must enter a smoothie title'
+            }            
         },         
         addIng () {
             if (this.another) {
