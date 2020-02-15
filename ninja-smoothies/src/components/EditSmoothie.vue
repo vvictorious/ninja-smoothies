a49dec04c53ab26d56abd3c45454f6987d661a8f
vvictorious <vvictorious30@gmail.com>
2020-02-15 11:02:45 -0700

    getting the smoothie title from db for that smoothie when edit btn is clicked

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,16 +1,29 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit A Smoothie {{this.$route.params.smoothie_slug}}</h2>
+    <div v-if="smoothie" class="edit-smoothie container">
+        <h2>Edit {{smoothie.title}} Smoothie</h2>
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
     name: 'EditSmoothie',
     data () {
         return {
+            smoothie: null,
 
         }
+    },
+    created () {
+        let ref = db.collection('smoothies').where('slug', '==', this.$route.params.smoothie_slug)
+        ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                this.smoothie = doc.data()
+                this.smoothie.id = doc.id
+                console.log(this.smoothie)
+            })
+        })
     }
 }
 </script>
